0c0604d1aba42d3fd28c22eab60f18d4e99e7954
etherealharshita <41101208+harshita115@users.noreply.github.com>
2020-10-06 12:35:53 +0530

    Update a new method

--- hackerrank_listComprehensions.py
+++ hackerrank_listComprehensions.py
@@ -6,3 +6,20 @@ if __name__ == '__main__':
 
 arr=[[A,B,C] for A in range(x+1) for B in range(y+1) for C in range(z+1) if A+B+C!=n]
 print(arr)
+
+#second Method
+if __name__ == '__main__':
+    x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    # List Comprehensions in Python - Hacker Rank Solution START
+    output = [];
+    abc = [];
+    for X in range(x+1):
+        for Y in range(y+1):
+            for Z in range(z+1):
+                if X+Y+Z != n:
+                    abc = [X,Y,Z];
+                    output.append(abc);
+print(output);
